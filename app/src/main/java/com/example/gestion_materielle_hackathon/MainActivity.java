bf59c2f9019979c3e0196b9277f4cbe6557fb36b
othsoh <oth1soh2@gmail.com>
2024-04-23 00:13:29 +0100

    Bottom_nav

--- app/src/main/java/com/example/gestion_materielle_hackathon/MainActivity.java
+++ app/src/main/java/com/example/gestion_materielle_hackathon/MainActivity.java
@@ -1,15 +1,49 @@
 package com.example.gestion_materielle_hackathon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment homeFragment = new Home();
+    Fragment supervisionFragment = new Supervision();
+    Fragment classificationFragment = new Classification();
+    Fragment actionFragment = new Action();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setItemIconTintList(null);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.iHome) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, homeFragment).commit();
+                return true;
+            } else if (item.getItemId() == R.id.iFriends) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, supervisionFragment).commit();
+                return true;
+            }else if (item.getItemId() == R.id.iGroup) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, classificationFragment).commit();
+                return true;
+            }else if (item.getItemId() == R.id.iProfile) {
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, actionFragment).commit();
+                return true;
+            }
+
+            return false;
+        });
     }
+
 }
