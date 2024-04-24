6faf44d7405793bda05d52b202d1ec2dca4f8667
WebMorphers <72748046+somoncef@users.noreply.github.com>
2024-04-24 23:06:26 +0100

    k

--- app/src/main/java/com/example/gestion_materielle_hackathon/Supervision.java
+++ app/src/main/java/com/example/gestion_materielle_hackathon/Supervision.java
@@ -1,64 +1,88 @@
 package com.example.gestion_materielle_hackathon;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.example.gestion_materielle_hackathon.model.LampMarkerOptions;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
-
+import androidx.fragment.app.FragmentManager;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.MarkerOptions;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Supervision#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Supervision extends Fragment {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.Random;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public Supervision() {
-        // Required empty public constructor
-    }
+public class Supervision extends Fragment  implements OnMapReadyCallback {
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Supervision.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Supervision newInstance(String param1, String param2) {
-        Supervision fragment = new Supervision();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private SupportMapFragment mapFragment;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getActivity());
+        if (ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            mapFragment.getMapAsync(this);
         }
     }
 
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_supervision, container, false);
+
+
     }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng targetLocation = new LatLng(35.5851307, -5.4077635);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(targetLocation, 15));
+
+        BitmapDescriptor lampOnIcon = BitmapDescriptorFactory.fromResource(R.drawable.lampon);
+        BitmapDescriptor lampOffIcon = BitmapDescriptorFactory.fromResource(R.drawable.lampoff);
+
+        double range = 0.01;
+
+        Random random = new Random();
+        for (int i = 0; i < 15; i++) {
+            double randomLat = targetLocation.latitude + range * (random.nextDouble() * 2 - 1);
+            double randomLng = targetLocation.longitude + range * (random.nextDouble() * 2 - 1);
+            LatLng randomLocation = new LatLng(randomLat, randomLng);
+            LampMarkerOptions markerOptions = new LampMarkerOptions()
+                    .position(randomLocation)
+                    .icon(i % 2 == 0 ? lampOnIcon : lampOffIcon)
+                    .id("lamp" + i)
+                    .lampType(i % 2 == 0 ? "on" : "off");
+            mMap.addMarker(markerOptions);
+        }
+    }
+
 }
